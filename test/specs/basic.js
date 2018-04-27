6c489c1cc267eb76b8d579e849fe429e06052c29
Rob Dodson <robdodson@google.com>
2018-04-27 16:07:01 -0700

    Fixup quotes

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -13,13 +13,13 @@ function isUnfocusable(el) {
   return true;
 }
 
-describe("Basic", function() {
+describe('Basic', function() {
   before(function() {
-    fixture.setBase("test/fixtures");
+    fixture.setBase('test/fixtures');
   });
 
   beforeEach(function() {
-    fixture.load("basic.html");
+    fixture.load('basic.html');
     // Because inert relies on MutationObservers,
     // wait till next microtask before running tests.
     return Promise.resolve();
@@ -29,38 +29,38 @@ describe("Basic", function() {
     fixture.cleanup();
   });
 
-  it("should have no effect on elements outside inert region", function() {
-    const button = fixture.el.querySelector("#non-inert");
+  it('should have no effect on elements outside inert region', function() {
+    const button = fixture.el.querySelector('#non-inert');
     expect(isUnfocusable(button)).to.equal(false);
   });
 
-  it("should make implicitly focusable child not focusable", function() {
-    const button = fixture.el.querySelector("[inert] button");
+  it('should make implicitly focusable child not focusable', function() {
+    const button = fixture.el.querySelector('[inert] button');
     expect(isUnfocusable(button)).to.equal(true);
   });
 
-  it("should make explicitly focusable child not focusable", function() {
-    const div = fixture.el.querySelector("#fake-button");
-    expect(div.hasAttribute("tabindex")).to.equal(false);
+  it('should make explicitly focusable child not focusable', function() {
+    const div = fixture.el.querySelector('#fake-button');
+    expect(div.hasAttribute('tabindex')).to.equal(false);
     expect(isUnfocusable(div)).to.equal(true);
   });
 
-  it("should remove attribute and un-inert content if set to false", function() {
-    const inertContainer = fixture.el.querySelector("[inert]");
-    expect(inertContainer.hasAttribute("inert")).to.equal(true);
+  it('should remove attribute and un-inert content if set to false', function() {
+    const inertContainer = fixture.el.querySelector('[inert]');
+    expect(inertContainer.hasAttribute('inert')).to.equal(true);
     expect(inertContainer.inert).to.equal(true);
-    const button = inertContainer.querySelector("button");
+    const button = inertContainer.querySelector('button');
     expect(isUnfocusable(button)).to.equal(true);
 
     inertContainer.inert = false;
-    expect(inertContainer.hasAttribute("inert")).to.equal(false);
+    expect(inertContainer.hasAttribute('inert')).to.equal(false);
     expect(inertContainer.inert).to.equal(false);
     expect(isUnfocusable(button)).to.equal(false);
   });
 
-  it("should be able to be reapplied multiple times", function() {
-    const inertContainer = fixture.el.querySelector("[inert]");
-    const button = document.querySelector("[inert] button");
+  it('should be able to be reapplied multiple times', function() {
+    const inertContainer = fixture.el.querySelector('[inert]');
+    const button = document.querySelector('[inert] button');
     expect(isUnfocusable(button)).to.equal(true);
 
     inertContainer.inert = false;
@@ -76,10 +76,10 @@ describe("Basic", function() {
     expect(isUnfocusable(button)).to.equal(true);
   });
 
-  it("should apply to dynamically added content", function(done) {
-    const newButton = document.createElement("button");
-    newButton.textContent = "Click me too";
-    const inertContainer = fixture.el.querySelector("[inert]");
+  it('should apply to dynamically added content', function(done) {
+    const newButton = document.createElement('button');
+    newButton.textContent = 'Click me too';
+    const inertContainer = fixture.el.querySelector('[inert]');
     inertContainer.appendChild(newButton);
     // Wait for the next microtask to allow mutation observers to react to the DOM change
     Promise.resolve().then(() => {
