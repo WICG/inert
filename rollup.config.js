dcf3c4215a1de5dc254f5f818c8dd8a49ff9fefb
Rob Dodson <robdodson@google.com>
2018-04-30 13:56:04 -0700

    Setting up Babel. Always a joy.

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,31 @@
 import uglify from 'rollup-plugin-uglify';
+import babel from 'rollup-plugin-babel';
 
 export default [
   {
     input: 'src/inert.js',
     output: {
       file: 'dist/inert.js',
-      format: 'umd'
-    }
+      format: 'umd',
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+    ],
   },
   {
     input: 'src/inert.js',
     output: {
       file: 'dist/inert.min.js',
       format: 'umd',
-      sourcemap: true
+      sourcemap: true,
     },
-    plugins: [uglify()]
-  }
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      uglify(),
+    ],
+  },
 ];
