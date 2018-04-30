d204a27b45d69e5fafc9ec7768eb60d9c550e1b3
Rob Dodson <robdodson@google.com>
2018-04-30 14:06:23 -0700

    Remove ie9 and 10...for now.

--- karma.conf.js
+++ karma.conf.js
@@ -81,18 +81,18 @@ module.exports = function(config) {
         platform: 'OS X 10.12',
         version: 'latest',
       },
-      'SL_IE_9': {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 2008',
-        version: '9',
-      },
-      'SL_IE_10': {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 2012',
-        version: '10',
-      },
+      // 'SL_IE_9': {
+      //   base: 'SauceLabs',
+      //   browserName: 'internet explorer',
+      //   platform: 'Windows 2008',
+      //   version: '9',
+      // },
+      // 'SL_IE_10': {
+      //   base: 'SauceLabs',
+      //   browserName: 'internet explorer',
+      //   platform: 'Windows 2012',
+      //   version: '10',
+      // },
       'SL_IE_11': {
         base: 'SauceLabs',
         browserName: 'internet explorer',
