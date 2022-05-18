db512d2efb3536a9d84a1e8f936bf2c1bb34ee28
Ed Sanders <ejsanders@gmail.com>
2022-05-18 16:58:41 +0100

    Replace Element with HTMLElement where appropriate
    Fixes #181

--- test/specs/element.spec.js
+++ test/specs/element.spec.js
@@ -1,5 +1,5 @@
-describe('Element.prototype', function() {
-  it('should patch the Element prototype', function() {
-    expect(Element.prototype.hasOwnProperty('inert')).to.be.ok;
+describe('HTMLElement.prototype', function() {
+  it('should patch the HTMLElement prototype', function() {
+    expect(HTMLElement.prototype.hasOwnProperty('inert')).to.be.ok;
   });
 });
